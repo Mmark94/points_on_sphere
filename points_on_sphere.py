4fe65038555e2c5dc9e3f79268992aea783759f1
Marco Monti <marco.monti@postgrad.manchester.ac.uk>
2021-08-03 10:40:44 +0200

    I removed some test scripts

--- points_on_sphere.py
+++ points_on_sphere.py
@@ -4,38 +4,56 @@ import numpy as np
 import math
 import random
 
-# This draws a sphere
-phi = np.linspace(0, np.pi, 20)
-theta = np.linspace(0, 2 * np.pi, 40)
-x = np.outer(np.sin(theta), np.cos(phi))
-y = np.outer(np.sin(theta), np.sin(phi))
-z = np.outer(np.cos(theta), np.ones_like(phi))
+def random_points_on_sphere(points=500, wireframe=True, Show=True, GIF=True):
+    # This draws the wireframe of a sphere
+    phi = np.linspace(0, np.pi, 20)
+    theta = np.linspace(0, 2 * np.pi, 40)
+    x = np.outer(np.sin(theta), np.cos(phi))
+    y = np.outer(np.sin(theta), np.sin(phi))
+    z = np.outer(np.cos(theta), np.ones_like(phi))
 
+    # We store the coordinates of the points in these lists
+    x_list = []
+    y_list = []
+    z_list = []
+    for i in range(points):
+        # Pick random points
+        X = random.uniform(-1, 1)
+        Y = random.uniform(-1, 1)
+        Z = random.uniform(-1, 1)  # (0,1) generates points only in the Northern Hemisphere
 
-space = [-1, 1]
-points = 100
+        # This makes sure the points are on the surface of the sphere
+        Vector = (math.sqrt((X ** 2) + (Y ** 2) + (Z ** 2)))
+        x_list.append(X / Vector)
+        y_list.append(Y / Vector)
+        z_list.append(Z / Vector)
 
-x_list = []
-y_list = []
-z_list = []
-for i in range(points):
-    X = random.uniform(-1, 1)
-    Y = random.uniform(-1, 1)
-    Z = random.uniform(-1, 1)        # (0,1) generates points only in the Northern Hemisphere
-    Vector = (math.sqrt((X**2) + (Y**2) + (Z**2)))
-    x_list.append(X/Vector)
-    y_list.append(Y/Vector)
-    z_list.append(Z/Vector)
+    # Plot the data
+    fig, ax = plt.subplots(1, 1, subplot_kw={'projection': '3d', 'aspect': 'auto'}, figsize=(14, 14))
+    # Plot the wireframe of the sphere
+    if wireframe:
+        ax.plot_wireframe(x, y, z, color='k', rstride=1, cstride=1)
+    # Plot the points
+    ax.scatter(x_list, y_list, z_list, s=30, c='r', zorder=10)
 
+    # Create a random seed to save the image
+    random_seed = str(random.random())[2:6]
 
-fig, ax = plt.subplots(1, 1, subplot_kw={'projection':'3d', 'aspect':'auto'}, figsize=(14, 14))
-ax.plot_wireframe(x, y, z, color='k', rstride=1, cstride=1)
-ax.scatter(x_list, y_list, z_list, s=25, c='r', zorder=10)
-plt.savefig("media/Sphere_points_" + str(points) + ".png", format='png', dpi=150)
-plt.show()
+    plt.savefig("media/Sphere_points_" + str(points) + "_s" + random_seed + ".png", format='png', dpi=150)
+    if Show:
+        plt.show()
 
-def rotate(angle):
-    ax.view_init(azim=angle)
+    def rotate(angle):
+        ax.view_init(azim=angle)
 
-rot_animation = animation.FuncAnimation(fig, rotate, frames=np.arange(0, 362, 2), interval=100)
-rot_animation.save("media_gif/Sphere_points_" + str(points) + ".gif", dpi=150, writer="imagemagick")
+    if GIF:
+        # rot_animation = animation.FuncAnimation(fig, rotate, frames=np.arange(0, 362, 2), interval=100)     # Smooth and slow, but it is 70MB
+        rot_animation = animation.FuncAnimation(fig, rotate, frames=np.arange(0, 362, 5), interval=200)  # Fast, it is 30MB
+        rot_animation.save("media_gif/Sphere_points_" + str(points) + "_s" + random_seed + ".gif", dpi=150, writer="imagemagick")
+    plt.close()
+    return None
+
+# test the code
+if __name__ == "__main__":
+
+    random_points_on_sphere(points=500, wireframe=True, Show=True, GIF=False)
